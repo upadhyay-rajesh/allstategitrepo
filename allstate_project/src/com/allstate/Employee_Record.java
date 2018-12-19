86e1654a864e9ea33b913e5989a3a7ea9eb29662
rajesh <upadhyay.rajesh@rediffmail.com>
2018-12-19 11:00:22 +0530

    allstate first project

---
+++ allstate_project/src/com/allstate/Employee_Record.java
@@ -0,0 +1,14 @@
+package com.allstate;
+
+public class Employee_Record {
+
+	public static void main(String[] args) {
+		Employee_Record e=new Employee_Record();
+		e.add();
+
+	}
+	public void add(){
+		System.out.println("employee added");
+	}
+
+}
